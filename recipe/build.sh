17143e6cc05d92e763022ee68a351362d4e0b17c
Adrian Insaurralde Avalos <adrianinsaval@gmail.com>
2025-05-08 15:12:49 -0300

    fix distinfo copy

--- recipe/build.sh
+++ recipe/build.sh
@@ -78,7 +78,7 @@ rm ${SP_DIR}/testbinding*.so
 popd
 
 ${PYTHON} setup.py dist_info --build-type=pyside2
-cp -r PySide2-${PKG_VERSION}.dist-info "${SP_DIR}"/
+cp -r pyside2-${PKG_VERSION}.dist-info "${SP_DIR}"/
 
 pushd sources/pyside2-tools
 mkdir -p build && cd build
